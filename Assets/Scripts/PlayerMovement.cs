6095c26aa3ff01d2bc4670303256281e0672071c
Py <143103772+PyThePylon@users.noreply.github.com>
2023-10-29 17:31:18 -0700

    Movement update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
-    public float moveSpeed = 3.0f; // Adjust the speed to your liking
+    private Quaternion originalRotation;
+    public float moveSpeed = 5f; // Adjust the speed to your liking
+
+    public GameObject npcCube;
+    private bool isLookingAtNPC = false;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         rb = GetComponent<Rigidbody>();
+        originalRotation = transform.rotation;
     }
 
     void Update()
@@ -31,5 +35,33 @@ public class PlayerMovement : MonoBehaviour
 
         // Use Rigidbody.MovePosition to move the character
         rb.MovePosition(transform.position + moveAmount);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isLookingAtNPC)
+            {
+                // Release the camera control and resume player movement
+                isLookingAtNPC = false;
+                // Add code here to allow the player to resume control of the camera
+
+                // Reset the camera to its original rotation
+                transform.rotation = originalRotation;
+
+                // Unlock the cursor
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else if (Vector3.Distance(transform.position, npcCube.transform.position) < 2f)
+            {
+                // Look at the NPC
+                isLookingAtNPC = true;
+                // Make the player look at the NPC
+                transform.LookAt(npcCube.transform.position);
+
+                // Lock the cursor to the center of the screen
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
     }
 }
